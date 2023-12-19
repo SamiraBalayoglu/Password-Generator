38d5352994ba69f835d26bcb0d0333f7462c2bc0
SamiraBalayoglu <148396554+SamiraBalayoglu@users.noreply.github.com>
2023-12-19 23:18:31 +0000

    adding last funtions

--- script.js
+++ script.js
@@ -90,50 +90,65 @@ const upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
-  let pwLength = paseInt(prompt(How many characters would you like your password to be? (Must be more than 8 and less than 128))) {
-    if ()pwLength < 8 || pwLength > 128 {
-      alert("Invalid password length. Please make it between 8 and 128 characters!")
-      return
-    }
+  let pwLengthPrompt = prompt("How many characters would you like your password to be? (Must be more than 8 and less than 128)");
+  let pwLength = parseInt(pwLengthPrompt);
+  
+  if (pwLength < 8 || pwLength > 128) {
+    alert("Invalid password length. Please make it between 8 and 128 characters!")
+    return;
   }
-  letspecialChar = confirm("Would you like special characters in your password?")
-letnumericChar = confirm("Would you like numeric characters in your password?")
-letsuppericChar = confirm("Would you like upper case characters in your password?")
-letlowerCaseChar = confirm("Would you like lower case characters in your password?")
 
-let pwOptions = {
+  let specialChar = confirm("Would you like special characters in your password?");
+  let numericChar = confirm("Would you like numeric characters in your password?");
+  let suppericChar = confirm("Would you like upper case characters in your password?");
+  let lowerCaseChar = confirm("Would you like lower case characters in your password?");
+let pwoptions = { 
   length: pwLength,
   special: specialCharacters,
   number: numericCharacters,
   upper: upperCasedCharacters,
   lower: lowerCaseChar
+};
+  return pwoptions
 }
-return pwOptions
-}
-
-// Function for getting a random element from an array
-function getRandom(arr) {
-
-
-}
-
 
 // Function to generate password with user input
 function generatePassword() {
-let options =getPasswordsOptions()
+  let options = getPasswordOptions();
 
-// Array to contain one of each type of chosen character to ensure each will be used
-let guarnateedChar []
+  // Array to contain one of each type of chosen character to ensure each will be used
+  let guarnateedChar = [];
+let pwresult = []
+  let possibleChar = []
+  if (options.special){
+     possibleChar = possibleChar.concat(specialCharacters)
+    guarnateedChar.push(getRandom(specialCharacters));
+  }
+  if (options.upper){
+     possibleChar = possibleChar.concat(upperCasedCharacters)
+    guarnateedChar.push(getRandom(upperCasedCharacters));
+  }
+  if (options.lower){
+     possibleChar = possibleChar.concat(lowerCasedCharacters)
+    guarnateedChar.push(getRandom(lowerCasedCharacters));
+  }
+  if (options.number){
+     possibleChar = possibleChar.concat(numericCharacters)
+    guarnateedChar.push(getRandom(numericCharacters));
+  }
+  
+ for (let i = 0; i < options.length; i++) {
+  var singlechar = getRandom (possibleChar)
+  pwresult.push (singlechar)
+ }
+ for (let i = 0; i < guarnateedChar.length; i++) {
+  const char = guarnateedChar[i];
+  pwresult [i] = char
+  
+ }
 
-if (options.special){
-  possibleChar = possibleChar.concar(specialCharacters)
-  guarnateedChar.push(getRandom(specialCharacters))
+  return pwresult.join ('')
 }
-
-}
-
-// Get references to the #generate element
-var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
@@ -143,35 +158,20 @@ function writePassword() {
   passwordText.value = password;
 }
 
+// Get references to the #generate element
+var generateBtn = document.querySelector('#generate');
+
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
 
-const charOptions = [];
-const generatedPassword = '';
 // You can store the generatedPassword as a string and concat each character OR
 // as an array and push each character, then join once you have enough characters
 
-// Function to prompt user for password options
-function getPasswordOptions() {
-  // Prompt for password length
-  // At least 8 characters, no more than 128 characters
-  // Conditional to check that the number that was entered is in range
-  // Prompts store data as strings, so need to parse into a number
-  // If the user's input is out of range, either return out of the function or call the function again
-
-  // Confirm which character sets to use
-  // If the user answers false for all, either return out of the function or call the function again
-  
-  // Once they select a character set:
-  // Generate a random character for each selected character set
-  // Either push selected character sets to a mega-array of all selected characters
-  // OR you can keep the arrays separate and generate a random number to select the array and another to select the index
-  
-  // Once character sets are selected, move on to generating random characters
-}
-
 // Function for getting a random element from an array
 function getRandom(arr) {
+  let randomi = Math.floor (Math.random()*arr.length) 
+  let randomel = arr[randomi]
+  return randomel
   // Need a variable to hold the password as it's being generated
   // Need a variable to hold the index that's being generated
 
