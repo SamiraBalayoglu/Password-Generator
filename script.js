42e7bb7426ae12f1998b4e011146823f4a49a4c9
SamiraBalayoglu <148396554+SamiraBalayoglu@users.noreply.github.com>
2023-12-20 12:54:27 +0000

    updating js

--- script.js
+++ script.js
@@ -100,16 +100,18 @@ function getPasswordOptions() {
 
   let specialChar = confirm("Would you like special characters in your password?");
   let numericChar = confirm("Would you like numeric characters in your password?");
-  let suppericChar = confirm("Would you like upper case characters in your password?");
+  let upperCasedChar = confirm("Would you like upper case characters in your password?");
   let lowerCaseChar = confirm("Would you like lower case characters in your password?");
-let pwoptions = { 
-  length: pwLength,
-  special: specialCharacters,
-  number: numericCharacters,
-  upper: upperCasedCharacters,
-  lower: lowerCaseChar
-};
-  return pwoptions
+
+  let pwoptions = { 
+    length: pwLength,
+    special: specialChar,
+    number: numericChar,
+    upper: upperCasedChar,
+    lower: lowerCaseChar
+  };
+
+  return pwoptions;
 }
 
 // Function to generate password with user input
@@ -118,34 +120,38 @@ function generatePassword() {
 
   // Array to contain one of each type of chosen character to ensure each will be used
   let guarnateedChar = [];
-let pwresult = []
-  let possibleChar = []
+  let pwresult = [];
+  let possibleChar = [];
+
   if (options.special){
-     possibleChar = possibleChar.concat(specialCharacters)
+    possibleChar = possibleChar.concat(specialCharacters)
     guarnateedChar.push(getRandom(specialCharacters));
   }
+
   if (options.upper){
-     possibleChar = possibleChar.concat(upperCasedCharacters)
+    possibleChar = possibleChar.concat(upperCasedCharacters)
     guarnateedChar.push(getRandom(upperCasedCharacters));
   }
+
   if (options.lower){
-     possibleChar = possibleChar.concat(lowerCasedCharacters)
+    possibleChar = possibleChar.concat(lowerCasedCharacters)
     guarnateedChar.push(getRandom(lowerCasedCharacters));
   }
+
   if (options.number){
-     possibleChar = possibleChar.concat(numericCharacters)
+    possibleChar = possibleChar.concat(numericCharacters)
     guarnateedChar.push(getRandom(numericCharacters));
   }
   
- for (let i = 0; i < options.length; i++) {
-  var singlechar = getRandom (possibleChar)
-  pwresult.push (singlechar)
- }
- for (let i = 0; i < guarnateedChar.length; i++) {
-  const char = guarnateedChar[i];
-  pwresult [i] = char
-  
- }
+  for (let i = 0; i < options.length; i++) {
+    var singlechar = getRandom(possibleChar);
+    pwresult.push(singlechar);
+  }
+
+  for (let i = 0; i < guarnateedChar.length; i++) {
+    const char = guarnateedChar[i];
+    pwresult[i] = char;
+  }
 
   return pwresult.join ('')
 }
